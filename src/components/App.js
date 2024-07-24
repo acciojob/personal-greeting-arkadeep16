8dd83b5105a394baafa8ae22e312315f1fbb0ce8
arkadeep16 <paularka544@gmail.com>
2024-07-25 03:05:31 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-
 import React from "react";
+import { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
+
+  const [displayInput, setDisplayInput] = useState("");
+
+  function getInput(event) {
+    setDisplayInput(event.target.value);
+  }
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text"  onChange={getInput}></input>
+        <p>{displayInput}</p>
     </div>
   )
 }
